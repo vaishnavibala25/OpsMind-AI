4165fdc1c7d527982f1c4d573933f242203a63a1
Vaishnavi <vaishnavibalakrishnan2006@gmail.com>
2026-06-04 10:40:14 +0530

    Deployed backend url is added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://opsmind-backend-fyu8.onrender.com"
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default API;
